2fbfd2462f85ddeee7492484a787ac8a80f45138
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-07-05 14:50:40 +0500

    one section added

---
+++ src/components/ButtonTransparent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ButtonTransparent() {
+    return (
+        <>
+            <button className='py-[15px] px-[30px] border-2 font-bold uppercase leading-[17px] tracking-[1px] hover:bg-black hover:text-white border-black bg-transparent'>
+                See Product
+            </button>
+        </>
+    )
+}
+
+export default ButtonTransparent
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // components
-import { ProductDesc, SeeProduct } from '../components'
+import { ButtonTransparent, ProductDesc, SeeProduct } from '../components'
 
 // sectins
 import { Mark2, ShopLinks } from '../sections'
@@ -9,6 +9,7 @@ function Home() {
     <>
       <Mark2 />
       <ShopLinks />
+
       <section className='mb-[48px]'>
         <div className='site-container bg-[#D87D4A] rounded-lg'>
           <div className='w-full pl-[117px] pt-[96px] pr-[95px] flex items-center gap-[138px] text-white '>
@@ -24,6 +25,23 @@ function Home() {
             </ProductDesc>
           </div>
         </div>
+      </section>
+
+      <section className='site-container pl-[95px] py-[101px] bg-[url("../assets/bg-speaker.png")] mb-12'>
+        <ProductDesc>
+          <h1 className='font-bold text-[28px] leading-[38px] tracking-[2px] mb-8'>ZX7 SPEAKER</h1>
+          <ButtonTransparent />
+        </ProductDesc>
+      </section>
+
+      <section className='site-container grid grid-cols-2 mb-[200px]'>
+        <img src="../assets/ear-phone-bitmap.png" alt="ear phone" />
+        <ProductDesc>
+          <div className='w-full h-full rounded-lg pt-[101px] pl-[95px] bg-[#F1F1F1]'>
+            <h1 className='font-bold text-[28px] leading-[38px] tracking-[2px] mb-8'>YX1 EARPHONES</h1>
+            <ButtonTransparent />
+          </div>
+        </ProductDesc>
       </section>
     </>
   )
